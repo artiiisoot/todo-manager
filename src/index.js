593450d89517afd9f116ad02357512d1f8806be0
shkim0060 <shkim0060@raoncorp.com>
2024-08-27 17:35:55 +0900

    commtit

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import "./assets/css/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./firebase";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
